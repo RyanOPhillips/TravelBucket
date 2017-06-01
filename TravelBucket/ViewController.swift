993e3898f6678a63935ef145469c3dd9225352f4
Ryan Phillips <rphillips4417@gmail.com>
2017-06-01 11:02:28 -0600

    Initial Commit

---
+++ TravelBucket/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TravelBucket
+//
+//  Created by Ryan Phillips on 6/1/17.
+//  Copyright © 2017 Ryan Phillips. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
