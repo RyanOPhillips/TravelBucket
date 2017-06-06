bdde7983fa5a43ccb190fdad2e82325dfdafd717
Ryan Phillips <rphillips4417@gmail.com>
2017-06-06 15:33:12 -0600

    Touch ID working

---
+++ TravelBucket/Image+CoreDataProperties.swift
@@ -0,0 +1,39 @@
+//
+//  Image+CoreDataProperties.swift
+//  TravelBucket
+//
+//  Created by Ryan Phillips on 6/6/17.
+//  Copyright © 2017 Ryan Phillips. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+
+extension Image {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<Image> {
+        return NSFetchRequest<Image>(entityName: "Image")
+    }
+
+    @NSManaged public var image: NSObject?
+    @NSManaged public var toItem: NSSet?
+
+}
+
+// MARK: Generated accessors for toItem
+extension Image {
+
+    @objc(addToItemObject:)
+    @NSManaged public func addToToItem(_ value: Item)
+
+    @objc(removeToItemObject:)
+    @NSManaged public func removeFromToItem(_ value: Item)
+
+    @objc(addToItem:)
+    @NSManaged public func addToToItem(_ values: NSSet)
+
+    @objc(removeToItem:)
+    @NSManaged public func removeFromToItem(_ values: NSSet)
+
+}
---
+++ TravelBucket/Item+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Item+CoreDataClass.swift
+//  TravelBucket
+//
+//  Created by Ryan Phillips on 6/6/17.
+//  Copyright © 2017 Ryan Phillips. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+@objc(Item)
+public class Item: NSManagedObject {
+
+}
