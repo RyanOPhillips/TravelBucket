ad5e4722430db09435b66c5b215c7aad52f757df
Ryan Phillips <rphillips4417@gmail.com>
2017-06-07 14:20:48 -0600

    Table view shows when Go To Bucket List button is pressed

---
+++ TravelBucket/ItemCell.swift
@@ -0,0 +1,24 @@
+//
+//  ItemCell.swift
+//  TravelBucket
+//
+//  Created by Ryan Phillips on 6/7/17.
+//  Copyright © 2017 Ryan Phillips. All rights reserved.
+//
+
+import UIKit
+
+class ItemCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
