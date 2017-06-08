393a17ab6f13e9c162dd8e7d3a2b8160817b0f7a
Ryan Phillips <rphillips4417@gmail.com>
2017-06-08 11:03:57 -0600

    More styling and beginning of button functionality

--- TravelBucket/ItemDetailsVC.swift
+++ TravelBucket/ItemDetailsVC.swift
@@ -10,26 +10,32 @@ import UIKit
 
 class ItemDetailsVC: UIViewController {
 
+    
+    @IBOutlet weak var dateField: UITextField!
+    @IBOutlet weak var notesField: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        
+        if let topItem = self.navigationController?.navigationBar.topItem {
+            topItem.backBarButtonItem = UIBarButtonItem(title: "Back", style: UIBarButtonItemStyle.plain, target: nil, action: nil)
+        }
 
         // Do any additional setup after loading the view.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func savePressed(_ sender: UIButton) {
+        
+        let item = Item(context: context)
+        
+        
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+    
+    
+    
+    
 }
