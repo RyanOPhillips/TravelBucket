da4c5e7edb4eaf2868bfe096e5c9f5b80a09d77c
Ryan Phillips <rphillips4417@gmail.com>
2017-06-12 10:22:09 -0600

    Added expanded view. Working on date picker

--- TravelBucket/ItemDetailsVCCompleted.swift
+++ TravelBucket/ItemDetailsVCCompleted.swift
@@ -8,31 +8,46 @@
 
 import UIKit
 
-class ItemDetailsVCCompleted: UIViewController {
+class ItemDetailsVCCompleted: UIViewController{
 
     
-//    @IBOutlet weak var notesField: UITextField!
-    
+    @IBOutlet weak var notesField: UITextField!
+//    @IBOutlet weak var datePicker: UIDatePicker!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         
+        
         if let topItem = self.navigationController?.navigationBar.topItem {
             topItem.backBarButtonItem = UIBarButtonItem(title: "Back", style: UIBarButtonItemStyle.plain, target: nil, action: nil)
         }
+        
+//        datePicker.delegate = self
+//        datePicker.datasource = self
 
         // Do any additional setup after loading the view.
     }
+    
+//    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+//        <#code#>
+//    }
+//    
+//    func 
+    
+    
     
 //    @IBAction func savePressed(_ sender: UIButton) {
 //        
 //        let item = Item(context: context)
 //        
+//        let notes = notesField.text
+//            item.notes = notes
+//        
 //        
 //        
 //    }
-    
+//    
     
     
     
---
+++ TravelBucket/ResizedMenuButtons.swift
@@ -0,0 +1,11 @@
+//
+//  ResizedMenuButtons.swift
+//  TravelBucket
+//
+//  Created by Ryan Phillips on 6/11/17.
+//  Copyright © 2017 Ryan Phillips. All rights reserved.
+//
+
+import UIKit
+
+
