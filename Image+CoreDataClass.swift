6df1359c0a5ced545d8170f955bbc3c9bf78c13e
Ryan Phillips <rphillips4417@gmail.com>
2017-06-14 18:50:51 -0600

    All fields and data working except for placeholder image

---
+++ Image+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Image+CoreDataClass.swift
+//  
+//
+//  Created by Ryan Phillips on 6/14/17.
+//
+//
+
+import Foundation
+import CoreData
+
+
+public class Image: NSManagedObject {
+
+}
---
+++ Item+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Item+CoreDataClass.swift
+//  
+//
+//  Created by Ryan Phillips on 6/14/17.
+//
+//
+
+import Foundation
+import CoreData
+
+
+public class Item: NSManagedObject {
+
+}
